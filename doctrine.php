c8fb7be6162a75963fc016e2708fb4601ed6a21d
Leticia <20241pal10130021@estudantes.ifpr.edu.br>
2026-07-01 23:12:42 -0300

    Preparar projeto para deploy no Render (TiDB + Docker)

--- doctrine.php
+++ doctrine.php
@@ -5,6 +5,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
 use utils\Conexao;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 ConsoleRunner::run(
     new SingleManagerProvider(Conexao::getEntityManager())
